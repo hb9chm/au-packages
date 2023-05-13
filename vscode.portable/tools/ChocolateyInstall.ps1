c20dfed9726eeec69a6af1434d0762cb8f726db8
hb9chm <stephan.enzler@gmx.ch>
2023-05-13 17:48:12 +0200

    vscode.portable

---
+++ vscode.portable/tools/ChocolateyInstall.ps1
@@ -0,0 +1,42 @@
+﻿
+$ErrorActionPreference = 'Stop'
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$PackageName = 'vscode'
+
+$File32  = (Join-Path $toolsDir "vscode_portable_x32.zip")
+$File64  = (Join-Path $toolsDir "vscode_portable_x64.zip")
+
+$ChecksumType32 = 'sha256'
+$Checksum32 = '8E2840C2807AF33A3053EB0A704FFD618AABF8D04CC9498F71073553782E0014'
+
+$ChecksumType64 = 'sha256'
+$Checksum64 = 'B9CFD4A5A4514635CEEF68FA557BF034E86FA7EE27CF344F9DC1C34E65147A87'
+$InstallationPath = Join-Path $(Get-ToolsLocation) $PackageName
+$DataPath = Join-Path $InstallationPath 'data'
+
+Get-ChildItem -Path $InstallationPath -Exclude data -ErrorAction Ignore | Remove-Item -Recurse -Force -ErrorAction Ignore
+New-Item -ItemType Directory -Path $InstallationPath -Force -ErrorAction Ignore
+New-Item -ItemType Directory -Path $DataPath -Force -ErrorAction Ignore
+
+$PackageArgs = @{
+    PackageName    = $PackageName
+    File           = $File32
+    ChecksumType   = $ChecksumType32
+    Checksum       = $Checksum32
+    File64         = $File64
+    ChecksumType64 = $ChecksumType64
+    Checksum64     = $Checksum64
+    UnzipLocation  = $InstallationPath
+}
+Install-ChocolateyZipPackage @PackageArgs
+
+$BinPath = Join-Path $InstallationPath "bin\code.cmd"
+Install-BinFile -Name Code -Path $BinPath
+$BinPath = Join-Path $InstallationPath "Code.exe"
+$LinkPath = Join-Path $([Environment]::GetFolderPath("CommonDesktopDirectory")) "Visual Studio Code.lnk"
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    $LinkPath = Join-Path $([Environment]::GetFolderPath("DesktopDirectory")) "Visual Studio Code.lnk"
+}
+Install-ChocolateyShortcut -ShortcutFilePath $LinkPath -TargetPath $BinPath -WorkingDirectory $InstallationPath
